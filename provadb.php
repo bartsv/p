78dcb3ce52eac104338a9e421161de0f61751519
bartsv <b.bartsv@gmail.com>
2015-04-25 15:47:04 +0200

    Add all the provadb php files

---
+++ provadb.php
@@ -0,0 +1,20 @@
+<?php
+function dbconn(){
+// collegamento al database
+$col = 'mysql:host=localhost;dbname=imotocro_forum';
+
+// blocco try per il lancio dell'istruzione
+try {
+  // connessione tramite creazione di un oggetto PDO
+  $db = new PDO($col , 'imoto_forum', '100289ba]');
+echo 'qq';
+return $db;
+}
+// blocco catch per la gestione delle eccezioni
+catch(PDOException $e) {
+  // notifica in caso di errorre
+  echo 'Attenzione: '.$e->getMessage();
+return null;
+}
+}
+?>
